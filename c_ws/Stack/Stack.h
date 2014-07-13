d8d856cd935956d48a0c3d4d25d6c8a16cb03103
borcun <borcunozkablan@gmail.com>
2014-07-13 17:45:47 +0300

    stack c source code

---
+++ c_ws/Stack/Stack.h
@@ -0,0 +1,35 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdio.h>
+
+#define MAX_SIZE 100
+#define TRUE 1
+#define FALSE 0
+
+// stack structure
+typedef struct Stack
+{
+  int *data;
+  int size;
+  int top_of_stack;
+} Stack;
+
+typedef struct Stack *StackPtr;
+
+// function that initialize stack
+int init(StackPtr stack, int _size);
+// function that checks whether stack is empty
+int isEmpty(Stack stack);
+// function that gets elements count of stack
+int size(Stack stack);
+// function that pops the top element from stack
+int pop(StackPtr stack);
+// function that pushs an element into stack
+int push(StackPtr stack, int elem);
+// function that deallocates memory
+void dealloc(Stack stack);
+// function that prints content of stack
+void print(Stack stack);
+
+#endif

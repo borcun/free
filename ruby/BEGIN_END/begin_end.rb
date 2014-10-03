487a6352cb3e67267dfda24501c97b580fc176be
borcun <borcunozkablan@gmail.com>
2014-10-03 17:02:04 +0300

    ruby

---
+++ ruby/BEGIN_END/begin_end.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+END {
+  puts "This is END block";
+}
+
+puts "This is content of program";
+
+BEGIN {
+  puts "This is BEGIN block";
+}
---
+++ ruby/comment/comment.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+# The sharp sign is comment operator of Ruby.
+name = "Ruby on Rails " # print Ruby on Rails
+
+=begin
+These lines
+are
+block comment
+of
+Ruby
+=end
+
+puts name;

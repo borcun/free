36d0749087736a09b683d3544092fabdb618d6b6
borcun <borcunozkablan@gmail.com>
2014-12-03 22:54:00 +0200

    ruby array

--- ruby/array/array.rb
+++ ruby/array/array.rb
@@ -1,7 +1,23 @@
 #!/usr/bin/ruby
 
-array = ["burak", 28, 3.14, "ozkablan"]
+arr = Array.new(5) { |i| i = 0}
+array = ["burak", 28, 3.14, "orcun"]
+digits = Array(0...10)
+x = "-----------------"
 
 array.each do |elem|
   puts elem
+end
+
+puts x
+
+for i in 0...arr.length
+  puts "#{arr[i]}"
+  arr[i] = i
+end
+
+puts
+
+for i in 0...arr.length
+  puts "#{arr.at(i)}"
 end

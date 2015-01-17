357f4fb1915d22d6ce9bb9d8ca3ce051fb799a93
borcun <borcunozkablan@gmail.com>
2015-01-17 13:12:35 +0200

    perl hello world

---
+++ perl/hello_world/hello_world.pl
@@ -0,0 +1,18 @@
+#!/opt/local/bin/perl
+
+# this line is comment line. the sharp operator is used for one line comment.
+
+=begin
+		These lines are comment lines as well. the assignment and begin keyword
+		starts multi-line comment lines, assignment and cut keyword ends comment
+		lines.
+
+		In Perl, function calls are made both using parenthesis or not. 
+		Both calls are valid. Besides, Perl file extension is .pl or .PL.
+
+		The first line is shebang which indicates perl binary file path, so
+		if the file execution permission is set, it can be executed with ./.
+=cut
+
+print "Hello World without parenthesis\n";
+print("Hello World with parenthesis\n");

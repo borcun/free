686f71eebc019c8afc41bb8f9af6ffd1dc382970
borcun <borcunozkablan@gmail.com>
2015-10-12 16:42:56 +0300

    hello_module

---
+++ linux/ldd/hello_world/.tmp_versions/hello_world.mod
@@ -0,0 +1,2 @@
+/home/boo/Desktop/my_ws/modules/hello_world.ko
+/home/boo/Desktop/my_ws/modules/hello_world.o
---
+++ linux/ldd/hello_world/hello_world.c
@@ -0,0 +1,15 @@
+#include <linux/init.h>
+#include <linux/module.h>
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int hello_init( void ) {
+	printk(KERN_ALERT "Hello World\n");
+	return 0;
+}
+
+static void hello_exit( void ) {
+	printk("Goodbye World\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);

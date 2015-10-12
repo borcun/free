40c215a1154d4e0d018a73b1ad5c30387107aec3
borcun <borcunozkablan@gmail.com>
2015-10-12 16:43:35 +0300

    modules

--- linux/ldd/hello_world/.hello_world.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/boo/Desktop/my_ws/modules/hello_world.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/boo/Desktop/my_ws/modules/hello_world.ko /home/boo/Desktop/my_ws/modules/hello_world.o /home/boo/Desktop/my_ws/modules/hello_world.mod.o
--- linux/ldd/hello_world/.tmp_versions/hello_world.mod
+++
@@ -1,2 +0,0 @@
-/home/boo/Desktop/my_ws/modules/hello_world.ko
-/home/boo/Desktop/my_ws/modules/hello_world.o

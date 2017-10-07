0658a5ba0c930b1a88dbb12170563535a328b3fb
borcun <borcunozkablan@gmail.com>
2017-10-07 13:11:47 +0300

    fix

--- python/file_ops/2/test.py
+++
@@ -1,9 +0,0 @@
-file = open( "test.txt", "r" )
-
-while True:
-	line = file.readline()
-
-	if line == '':
-		break
-	else:
-		print( line )
--- python/samples/while/while_else.py
+++
@@ -1,6 +0,0 @@
-condition = False
-
-while condition :
-	print( "hello world" )
-else:
-	print( "good bye world" )

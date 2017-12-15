89f1587de3b1e79b41b0194fcd775fff3ce77381
borcun <borcunozkablan@gmail.com>
2017-12-15 23:34:15 +0300

    open window with sfml

---
+++ cpp/SFML/OpenWindow/open_window.cpp
@@ -0,0 +1,30 @@
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <unistd.h>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+
+int main() {
+  RenderWindow window;
+
+  window.create( VideoMode( 800, 600 ), "my window" );
+  window.setPosition( Vector2i( 100, 100 ) );
+  
+  while( window.isOpen() ) {
+    Event event;
+
+    while( window.pollEvent( event ) ) {
+      if( Event::Closed == event.type ) {
+	window.close();
+      }
+
+      window.clear( Color::Black );
+      window.display();
+    }
+  }
+  
+  return 0;
+}

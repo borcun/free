21a8b16445ac4400e67f25ce1d320178dd21d4f2
orcun ozkablan <borcunozkablan@gmail.com>
2019-07-11 22:30:09 +0300

    rrq is tested

--- etftp/main.cpp
+++ etftp/main.cpp
@@ -7,7 +7,8 @@ int main( int argc, char **argv ) {
   ETFTPError err;
   ETFTPData data;
   ETFTPOAck oack;
-
+  
+  std::cout << "**************************" << std::endl;
   std::cout << "RRQ Op Code  : " << rrq.opcode  << std::endl;
   std::cout << "WRQ Op Code  : " << wrq.opcode  << std::endl;
   std::cout << "Data Op Code : " << data.opcode << std::endl;
@@ -22,6 +23,22 @@ int main( int argc, char **argv ) {
   std::cout << "Error Length: " << err.len  << std::endl;
   std::cout << "OACK Length : " << oack.len << std::endl;
   std::cout << "**************************" << std::endl;
+
+  char *stream = new char[ rrq.len ];
+  int len = 0;
+  
+  rrq.filename_len = 9;
+  memcpy( rrq.filename, "hello.txt", rrq.filename_len );
+  rrq.mode_len = 8;
+  memcpy( rrq.mode, MODE_NETASCII, rrq.mode_len );
+
+  len = rrq.serialize( stream );
+
+  for( int i=0; i < len; ++i ) {
+    printf( "%02x ", ( int ) stream[i] );
+  }
+
+  std::cout << std::endl;
   
   return 0;
 }

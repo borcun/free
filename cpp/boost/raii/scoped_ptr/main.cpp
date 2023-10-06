6ab7e1837ce9091468b7c1d02b48a3cef7cc357d
boo <borcunozkablan@gmail.com>
2023-10-07 00:14:51 +0300

    scoped_ptr is used for user-define type

--- cpp/boost/raii/scoped_ptr/main.cpp
+++ cpp/boost/raii/scoped_ptr/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <boost/scoped_ptr.hpp>
 
+class ScopedPtrTest {
+public:
+  ScopedPtrTest() { std::cout << __FUNCTION__ << std::endl; }
+  ~ScopedPtrTest() { std::cout << __FUNCTION__ << std::endl; }
+};
+
 int main() {
   boost::scoped_ptr<int> iptr {new int{1}};
-
+  boost::scoped_ptr<ScopedPtrTest> spt_ptr {new ScopedPtrTest};
+  
   std::cout << "*iptr: " << *iptr << std::endl;
   iptr.reset(new int{2});
   std::cout << "*iptr.get(): " << *iptr.get() << std::endl;

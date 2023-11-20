f779aa643932b120634195cd4b5f4ef63e137ca1
boo <borcunozkablan@gmail.com>
2023-11-20 14:55:07 +0300

    integer overflow samples is improved

--- c/infosec/6. Integer Overflow/main.c
+++ c/infosec/6. Integer Overflow/main.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-  size_t limit = 10;
+/*
+ * the return type is size_t, because it's also return type of strlen.
+ * size_t is return type frequently used in C programming language.
+ */
+size_t histogram(const char * const str) {
+  return NULL == str ? 0 : strlen(str);
+}
 
-  // what if replace greater operator (>) with greater and equal operator (>=)
-  while (limit > 0) {
-    printf("limit: %zu\n", limit);
-    --limit;
+int main(int argc, char **argv) {
+  size_t len = 0;
+  
+  if (2 != argc) {
+    fprintf(stdout, "usage: %s\n", "./int-oflow <word>");
+    return -1;
   }
+
+  len = histogram(argv[1]);
+
+  printf("%s: ", argv[1]);
+  
+  // what if replace greater operator (>) with greater and equal operator (>=)
+  while (len-- > 0) {
+    printf("%c", '*');
+  }
+
+  puts("");
 
   return 0;
 }

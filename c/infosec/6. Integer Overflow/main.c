04e29ead5b728930f2ae90c642f4516e3b07f772
boo <borcunozkablan@gmail.com>
2023-11-20 14:44:54 +0300

    d change to zu

--- c/infosec/6. Integer Overflow/main.c
+++ c/infosec/6. Integer Overflow/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-  unsigned int limit = 10;
+  size_t limit = 10;
 
   // what if replace greater operator (>) with greater and equal operator (>=)
   while (limit > 0) {
-    printf("limit: %d\n", limit);
+    printf("limit: %zu\n", limit);
     --limit;
   }
 

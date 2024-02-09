e7ec1ff04a07621d45af36ae9378645c0d7610b5
borcun <borcunozkablan@gmail.com>
2024-02-10 00:28:50 +0300

    credit card validation example added

---
+++ haskell/cc-validation/ccval.hs
@@ -0,0 +1,28 @@
+-- function that converts an integer to a list including digits of the integer
+toDigits :: Int -> [Int]
+toDigits num
+  | num < 10 = [num]
+  | otherwise = (toDigits (num `div` 10)) ++ [num `mod` 10]
+
+-- function that doubles numbers of the list, whose indices are odd
+toDoubleEveryOther :: [Int] -> [Int]
+toDoubleEveryOther nums
+  | 1 == length(nums) = [2 * (head nums)]
+  | 0 == (length nums) `mod` 2 = (toDoubleEveryOther (init nums)) ++ [last nums]
+  | otherwise = (toDoubleEveryOther (init nums)) ++ [2 * last nums]
+
+-- a utility function that gets sum of digits of an integer
+sumDigitsUtil :: Int -> Int
+sumDigitsUtil num
+  | num < 10 = num
+  | otherwise = (num `mod` 10) + sumDigitsUtil (num `div` 10)
+
+-- function that gets sum of digits processed of the credit card
+sumDigits :: [Int] -> Int
+sumDigits nums
+  | 1 == (length nums) = sumDigitsUtil (head nums)
+  | otherwise = sumDigitsUtil (head nums) + sumDigits (tail nums)
+
+-- validation function for the credit card
+validate :: Int -> Int
+validate num = (sumDigits (toDoubleEveryOther (toDigits num))) `mod` 10

d21208a911e07c3664b554c2ea078b59678cc02c
borcun <borcunozkablan@gmail.com>
2024-02-11 21:20:10 +0300

    a few examples added

--- haskell/function/function.hs
+++ haskell/function/function.hs
@@ -7,7 +7,11 @@ isEven n
   | n `mod` 2 == 0 = True
   | otherwise = False
 
+f :: Int -> Int -> Int -> Int
+f x y z = x + y + z
+
 main = do
   print (sumtorial 10)
   print (isEven 1)
   print (isEven 2)
+  print (f 3 4 5)
